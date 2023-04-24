0ae1c4504894e91bdf680d7f37b107fa6e919101
wooseok <parkdntjr@naver.com>
2023-04-25 01:04:15 +0900

    정신차리고 일찍 자자

--- 230424 Cpp inflearn sol 30/Source 30.cpp
+++ 230424 Cpp inflearn sol 30/Source 30.cpp
@@ -1,17 +1,19 @@
 #include<iostream>
 using namespace std;
 int main() {
-	int N;
-	int cnt = 0;
+	long long int N;
+	long long int tmp;
+	long long int decimal = 1;;
+	long long int cnt = 0;
 	cin >> N;
-	for (int i = 0; i < N; i++) {
-		int tmp = i;
-		while (tmp > 0) {
-			if (tmp % 10 == 3) {
-				cnt++;
-			}
-			tmp /= 10;
-		}
+	tmp = N;
+	while (tmp > 0) {
+		if (tmp % 10 >= 3)
+			cnt += (tmp / 10 + 1) * decimal;
+		else
+			cnt += tmp / 10 * decimal;
+		tmp /= 10;
+		decimal *= 10;
 	}
 	cout << cnt;
 	return 0;
