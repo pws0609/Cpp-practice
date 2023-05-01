beed8ed538e868752085644568e6315d6c91f37b
wooseok <parkdntjr@naver.com>
2023-05-01 23:39:27 +0900

    조금 더 수학적으로 접근해보자

--- 230501 Cpp inflearn sol 40~41/Source 41.cpp
+++ 230501 Cpp inflearn sol 40~41/Source 41.cpp
@@ -4,34 +4,40 @@ int main() {
 	int N;
 	cin >> N;
 	int cnt = 0;
-	
-	for(int i = 3; i<N; i++){
+	int startnum = 0;
+	if (N % 2 == 1) {
+		cout << N / 2 << " + " << N / 2 + 1<< " = " << N << endl;
+		cnt++;
+	}
+	for (int i = 3; i < N; i++) {
 		//Â¦¼ö 
-		if(i%2 == 0){
+		if (i % 2 == 0) {
 			int divide = i / 2;
-			int neck = N / divide;
-			int startnum = (neck - 1) / 2 - divide + 1;
-			if(startnum <= 0)
+			if (N % divide != 0)
 				continue;
-			if(N % divide == 1){
+			int neck = N / divide;
+			startnum = (neck - 1) / 2 - divide + 1;
+			if (startnum <= 0)
+				continue;
+			if ((startnum * 2 + i - 1) * divide == N) {
 				cnt++;
 				cout << startnum;
-				for(int j = 1; j<i; j++){
+				for (int j = 1; j < i; j++) {
 					cout << " + " << startnum + j;
 				}
 				cout << " = " << N << endl;
 			}
 		}
 		//È¦¼ö 
-		else{
-			int num = N%i;
-			int startnum = (N/i)-(i-1)/2;
-			if(num!=0)
+		else {
+			int num = N % i;
+			int startnum = (N / i) - (i - 1) / 2;
+			if (num != 0)
 				continue;
-			if(startnum > 0){
+			if (startnum > 0) {
 				cnt++;
 				cout << startnum;
-				for(int j = 1; j<i; j++){
+				for (int j = 1; j < i; j++) {
 					cout << " + " << startnum + j;
 				}
 				cout << " = " << N << endl;
