ca0499113c834ca55a0b5e168a76501de42429c5
wooseok <parkdntjr@naver.com>
2023-05-10 01:07:16 +0900

    알고리즘 정립하고 문제풀이 들어가자

---
+++ sol 45. 230510/Source 45.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int N, K;
+	cin >> N >> K;
+	int* arr = new int[N];
+	for (int i = 0; i < N; i++)
+		arr[i] = i + 1;
+	int ptr = 0, endptr = N - 1;
+	int j = 0;
+	while (endptr > 0) {
+		for (int i = 0; i < K - 1; i++) {
+			if (ptr >= endptr)
+				ptr -= endptr + 1;
+			ptr++;
+		}
+		for (int i = ptr; i < endptr; i++) {
+			arr[i] = arr[i + 1];
+		}
+		endptr--;
+	}
+	cout << arr[0];
+}
