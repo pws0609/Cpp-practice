7de99d01e268ad78db1ff771b85b9e9724178656
unknown <parkdntjr@naver.com>
2023-05-12 23:31:22 +0900

    다 풀어놓고 뻘짓..

--- sol 46. 230511/Source 46.cpp
+++ sol 46. 230511/Source 46.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main() {
 	int N;
 	cin >> N;
-	int* work = new int[N];
+	int* work = new int[N+1]();
 	int sum = 0;
-	for (int i = 0; i < N; i++) {
+	for (int i = 1; i < N+1; i++) {
 		cin >> work[i];
 		sum += work[i];
 	}
@@ -13,25 +13,32 @@ int main() {
 	int K;
 	cin >> K;
 
-	if (sum < K) {
-		cout << -1;
+	if (sum <= K) {
+		cout << "-1";
 		return 0;
 	}
-	int ptr = 0;
+	
+	int ptr = 1;
 	for (int i = 0; i < K; i++) {
-		if (ptr == N)
-			ptr = 0;
+//		cout<<i<<":";
+//		for(int j = 1; j<N+1;j++){
+//			cout<<work[j]<<" ";
+//		}
+//		cout<<endl;
+		ptr++;
+		if (ptr >= N+1)
+			ptr = 1;
 		while (work[ptr] == 0) {
 			ptr++;
-			if (ptr == N)
-				ptr = 0;
+			if (ptr >= N+1)
+				ptr = 1;
 		}
-		work[ptr++]--;
+		work[ptr]--;
 	}
 	while (work[ptr] == 0) {
 		ptr++;
-		if (ptr == N)
-			ptr = 0;
+		if (ptr >= N+1)
+			ptr = 1;
 	}
 	cout << ptr;
 	return 0;
