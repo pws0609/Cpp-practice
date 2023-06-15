82f977b17e603e4080b1e6e2d95bded4510b9af7
wooseok <parkdntjr@naver.com>
2023-06-15 23:58:14 +0900

    응용력을 키우자

---
+++ sol 75. 230615~230616/Source 75.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main() {
+	int N, M, D;
+	priority_queue<pair<int,int>> pQ;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> M, D;
+	}
+	while (true) {
+
+	}
+}
