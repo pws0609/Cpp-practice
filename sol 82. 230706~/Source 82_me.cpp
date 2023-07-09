916ecc9342201b6ec2d951d17fc45e71e563132d
unknown <parkdntjr@naver.com>
2023-07-09 19:50:40 +0900

    열심히 하자

--- sol 82. 230706~/Source 82_me.cpp
+++ sol 82. 230706~/Source 82_me.cpp
@@ -1,22 +1,19 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-int N, R;
-int main() {
-	vector<vector<int>> res;
-	int tot = 1;
-	cin >> N >> R;
-	int* num = new int[N];
-	for (int i = 0; i < N; i++) {
-		cin >> num[i];
-	}
-	for (int i = 0; i < R; i++) {
-		tot *= (N - i);
-	}
-	while (R > 0) {
-		for (int i = 0; i < R+1; i++) { 
 
+void DFS(int a){
+	if(a == R){
+		for(int i = 0; i<a; i++){
+			cout<<res[i];
+		}
+		cout << endl;
+	}
+	else{
+		for(int i = 0; i < N; i++){
+			if(chk[i] == 0){
+				res[a] = num[i];
+				chk[i] = 0;
+				DFS(a+1);
+				chk[i] = 1;
+			}
 		}
 	}
-	return 0;
 }
